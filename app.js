3639b5ef74212408f1062af62a72527bcaec2f8c
Vishesh <visheshsingh16@gmail.com>
2018-12-31 17:00:53 -0500

    added another element

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ window.addEventListener('scroll', function (e) {
     const scrolled = window.pageYOffset;
     for (let i = 0; i < target.length; i++) {
         const pos = scrolled * target[i].dataset.rate;
-        target[i].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+        if (target[i].dataset.direction == 'vertical') {
+            target[i].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+        } else {
+            const posX = scrolled * target[i].dataset.ratex;
+            const posY = scrolled * target[i].dataset.ratey;
+            target[i].style.transform = `translate3d(${posX}px, ${posY}px, 0px)`;
+        }
     }
 
 })
